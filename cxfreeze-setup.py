79b0994afa80f018e765a36be730fc640d872bd2
Erik Nyquist <eknyquist@gmail.com>
2019-01-15 17:39:04 -0800

    Fix windows build script

--- cxfreeze-setup.py
+++ cxfreeze-setup.py
@@ -3,12 +3,12 @@ import cx_Freeze
 exe_options = {
     "build_exe": {
         "packages": ["pygame", "tones", "prompt_toolkit"],
-        "include_files": ["audio/"],
+		"include_files": ["example-map/"],
         "excludes": ["tkinter", "numpy", "scipy", "PyQt5", "distutils",
 			"unittest", "test", "email", "pkg_resources"]
     }
 }
 
-exes = [cx_Freeze.Executable("example_map.py")]
+exes = [cx_Freeze.Executable("scripts/run-example-map.py")]
 
 cx_Freeze.setup(name="test game", options=exe_options, executables=exes)
